a51a8064efb5079b2a1967a03a33852f40e39987
MSI <MSI@HOME-PC>
2024-10-20 21:22:23 +0300

    /

---
+++ View/MainFormView.cs
@@ -0,0 +1,21 @@
+﻿using DevExpress.XtraEditors;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JurnalTZ.View
+{
+    public partial class MainFormView : DevExpress.XtraEditors.XtraForm
+    {
+        public MainFormView()
+        {
+            InitializeComponent();
+        }
+    }
+}
